2b29e2f495c8573ca627cb29651a7194bad5be8d
Baileymomo25 <bailey.momo25@gmail.com>
2025-10-07 00:38:36 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const paymentRoutes = require('./routes/payments');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
